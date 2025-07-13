becc7367f3636fd0ddc243f0b812bd0a6819f862
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-07-13 14:02:05 +0700

    127.[Tailwind CSS #11] Tạo tailwind.config.js trong Tailwind V4 như thế nào

---
+++ 127.TAILWIND-CONFIG/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        tuychon: "#2233cc",
+        tuychon2: {
+          100: "#33ffcc",
+          500: "#22ffff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
